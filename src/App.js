7ab058715e2d85364afe5759677b956b319afa61
mad90990 <abdulaziz.a.a.2006@gmail.com>
2023-02-25 14:38:37 +0500

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Rasmcat from "./Section-5/section-1-abdulaziz";
-
+import RasmcatAA1qism from "./Section-5/section-1-abdulaziz";
+import Section6 from "./Section-6/Section-6";
 function App() {
   return (
     <div className="App">
-     <Rasmcat/>
-     {/* abdulaziz */}
+{/* abdulaziz */}
+     <RasmcatAA1qism/>
+
+    <Section6/>
     </div>
   );
 }
---
+++ src/Section-6/Section-6.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Section-6.css";
+import rasm from "./img-png-6/Ellipse 59.png";
+import rasm1 from "./img-png-6/Ellipse 60.png";
+import rasm2 from "./img-png-6/Ellipse 61.png";
+import rasm3 from "./img-png-6/Ellipse 62.png";
+import rasm4 from "./img-png-6/Ellipse 63.png";
+import rasm5 from "./img-png-6/Ellipse 65.png";
+let Section6 = () =>{
+    return(
+        <div className="bir-6">
+            <div className="container">
+            <text className="text-6">Testimonials</text>
+            <div className="rasmimg1">
+                <img className="rasm-1" src={rasm}/>
+                <img className="rasm-2" src={rasm1}/>
+                <div className="kot1"></div>
+                <div className="kot2"></div>
+                <img className="rasm-3" src={rasm2}/>
+                <div className="kot-4"></div>
+                <img className="rasm-5" src={rasm3}/>
+                <img className="rasm-6" src={rasm4}/>
+                <div className="kot-5"></div>
+                <img src={rasm5} className="AAA"/>
+            </div>
+            </div>
+        </div>
+    )
+ };
+
+
+ export default Section6;
